4221d06529b02671eda3fc60aba807fdaec249eb
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-12 12:35:30 +0700

    [FIX] Memperbaiki tampilan navbar

--- resources/views/layouts/landing/partials/navbar.blade.php
+++ resources/views/layouts/landing/partials/navbar.blade.php
@@ -13,14 +13,8 @@
                 <a href="contact.html" class="nav-item nav-link">Kontak</a>
             </div>
             <div class="d-flex m-3 me-0">
-                <a href="#" class="position-relative me-4 my-auto">
-                    <i class="fa fa-shopping-bag fa-2x"></i>
-                    <span
-                        class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1">
-                    </span>
-                </a>
-                <a href="{{ route('login.index') }}" class="my-auto">
-                    <i class="fas fa-user fa-2x"></i>
+                <a href="{{ route('login.index') }}" class="my-auto text-white btn btn-primary">
+                    Masuk
                 </a>
             </div>
         </div>
