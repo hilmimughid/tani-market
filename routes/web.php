6fd86121d2a6927ead1375123ce56bcac0f9c22a
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-12 12:00:45 +0700

    [CHORE] Add initial project files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriProdukController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PesananController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SlideshowController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +22,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/detail-produk', [HomeController::class, 'produkDetail'])->name('produk.detail');
+Route::resource('/register', RegisterController::class);
+Route::resource('/login', LoginController::class);
+Route::resource('/dashboard', DashboardController::class);
+Route::resource('/kategori-produk', KategoriProdukController::class);
+Route::resource('/produk', ProdukController::class);
+Route::resource('/pesanan', PesananController::class);
+Route::resource('/slideshow', SlideshowController::class);
+Route::resource('/user', UserController::class);
