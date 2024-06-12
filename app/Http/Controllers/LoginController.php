c20d57ede2da09ff4824f67be20ed21f061de141
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-12 20:05:01 +0700

    [FIX] Memperbaiki lokasi view login dan register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends Controller
      */
     public function index()
     {
-        return view('login');
+        return view('authentication.login');
     }
 
     public function login(Request $request)
