2d75a8523b87138b1a6a5b64627a7fde9d68fbdd
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-13 05:18:02 +0700

    [FIX] Memperbaiki footer

--- resources/views/layouts/landing/partials/footer.blade.php
+++ resources/views/layouts/landing/partials/footer.blade.php
@@ -15,22 +15,20 @@
                 <h4 class="text-light mb-3" id="lokasi">Lokasi Kami</h4>
                 <p style="text-align: justify;">Alamat: Jl. Raya Arjowinangun No.20 A, Kel. Arjowinangun, Kec.
                     Kedungkandang, Kota Malang</p>
-                <iframe
+                <iframe class="maps"
                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15802.508379934992!2d112.638408!3d-8.0373269!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6279950d6919f%3A0x7de8a98111ff6457!2sTANI%20MARKET!5e0!3m2!1sen!2sid!4v1718199564990!5m2!1sen!2sid"
-                    width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="col-lg-4 col-md-8">
             <div class="d-flex flex-column text-start footer-item">
                 <h4 class="text-light mb-3" id="kontak">Hubungi Kami</h4>
-                <a class="btn-link text-white d-flex align-items-center mb-2"
-                    href="https://api.whatsapp.com/send?phone=6281295903492&text=hallo+tani+market+saya+ingin+order%3F">
-                    <i class="text-primary fab fa-whatsapp fa-2x pe-3"></i>081295903492
+                <a class="btn-link text-white d-flex align-items-center mb-2" href="https://wa.me/87859198577">
+                    <i class="text-primary fab fa-whatsapp fa-2x pe-3"></i>087859198577
                 </a>
                 <a class="btn-link text-white d-flex align-items-center"
-                    href="https://www.instagram.com/tani_markets?igsh=dTF0cGVnYTg1ZGg2">
-                    <i class="text-danger fab fa-instagram fa-2x pe-3"></i>@tani_markets
+                    href="https://www.instagram.com/tanimarket.fresh?igsh=MWJ6dWlrZXp0YmU1dQ==">
+                    <i class="text-danger fab fa-instagram fa-2x pe-3"></i>@tanimarket.fresh
                 </a>
             </div>
         </div>
