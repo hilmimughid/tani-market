2f6cbf5f176a2b55dc63efe62daaf04b4611c59e
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-13 18:06:40 +0700

    [FIX] Memperbaiki tampilan login

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -16,11 +16,13 @@
                                 @csrf
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" name="email" class="form-control" id="email" required>
+                                    <input type="email" name="email" class="form-control" id="email"
+                                        placeholder="Masukkan Email" required>
                                 </div>
                                 <div class="mb-4">
                                     <label for="password" class="form-label">Password</label>
-                                    <input type="password" name="password" class="form-control" id="password" required>
+                                    <input type="password" name="password" class="form-control" id="password"
+                                        placeholder="Masukkan Password" required>
                                 </div>
                                 <button type="submit" class="btn btn-success w-100 py-8 fs-4 mb-4 rounded-2">Masuk</button>
                                 <div class="d-flex align-items-center justify-content-center">
